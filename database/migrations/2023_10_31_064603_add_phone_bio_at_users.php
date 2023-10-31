1878093e7950719fa4e3ff4595f012d5bf2c151a
omghon <shaipokun217@gmail.com>
2023-10-31 14:02:30 +0700

    pert 4

---
+++ database/migrations/2023_10_31_064603_add_phone_bio_at_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->after('email')->default('user');
+            $table->string('phone')->after('email')->nullable();
+            $table->text('bio')->after('phone')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('phone');
+            $table->dropColumn('bio');
+        });
+    }
+};

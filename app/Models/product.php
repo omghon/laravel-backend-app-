77435f2c0df3920965c22e5182432d4ec7cb2ae2
omghon <shaipokun217@gmail.com>
2023-12-26 17:05:25 +0700

    product_by_category

--- app/Models/product.php
+++ app/Models/product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,5 +21,9 @@ class Product extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeCategoryId(Builder $query, string $categoryId): Builder
+    {
+        return $query->where('category_id', 'LIKE', '%' . $categoryId . '%');
+    }
 }
-// 
